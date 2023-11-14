2092d18027b60d397abd3bc212c7d309ee4fd972
Konstantin <o.lover4@bk.ru>
2023-11-14 23:37:37 +0300

    #1.1

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
+    var modal = document.querySelector('.modal');
+    var startBtn = document.querySelector('.start');
+    var crossBtn = document.querySelector('.crosses');
+    var circleBtn = document.querySelector('.circles');
+    var boxes = document.querySelectorAll('.box');
+    var player = 'X';
 });
--- script.ts
+++ script.ts
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.querySelector('.modal');
+    const startBtn = document.querySelector('.start');
+    const crossBtn = document.querySelector('.crosses');
+    const circleBtn = document.querySelector('.circles');
+    const boxes = document.querySelectorAll('.box');
 
+    let player = 'X';
 });
